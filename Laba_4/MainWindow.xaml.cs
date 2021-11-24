4415f333ec10432ed1100927fc0a656b6fd2e569
Pyperdok <zhitov.egoriko@mail.ru>
2021-11-24 08:17:02 +0300

    Лабораторная работа #4

---
+++ Laba_4/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Laba_4
+{
+    //Для раздачи кино(рейтинг)
+    //Фильм(хронометраж, количество наград, тип (художественный, документальный и т.п.))
+    //Сериал(общее количество серий, количество сезонов)
+    //Телепередача(продолжительность, эфирное время)
+    public partial class MainWindow : Window
+    {
+        private Random random = new Random();
+        private List<Movie> movies = new List<Movie>();
+        private float RandomRating { get { return (float)random.Next(0, 100) / 10; } }
+        private int filmCount;
+        private int serialCount;
+        int tvProgramCount;
+
+        delegate Movie MovieDelegate(); //Делегает возращающий фильм или сериал или передачу
+
+        //Заполняет массив кино фильмами, сериалами, передачами
+        private void Fill(int movieCount, MovieDelegate movie)
+        {
+            for (int i = 0; i < movieCount; i++)
+            {
+                movies.Add(movie());
+            }
+        }
+
+        //Перезаполняет машину, загружая кино
+        private void BT_InitMovies(object sender, RoutedEventArgs e)
+        {
+            MovieLabel.Content = string.Empty;
+            BitmapImage none = new BitmapImage(new Uri("/Images/None.png", UriKind.Relative));
+            IMG_Icon.Source = none;
+
+            movies.Clear();
+            //Заполнение кино-автомата(Фильмы)
+            filmCount = random.Next(3, 12);
+            L_FilmCount.Content = $"Фильмов: {filmCount}";
+            //Заполнение кино-автомата(Сериалы)
+            serialCount = random.Next(2, 20);
+            L_SerialCount.Content = $"Сериалов: {serialCount}";
+            //Заполнение кино-автомата(ТВ Передача)
+            tvProgramCount = random.Next(1, 15);
+            L_TvCount.Content = $"Передач: {tvProgramCount}";
+
+            Fill(filmCount, () =>
+            {
+                string time = $"{random.Next(0, 60):D2}:{random.Next(0, 60):D2}";
+                return new Film(time, (Film.Genre)random.Next(0, 3), RandomRating);
+            });
+
+            Fill(serialCount, () =>
+            {
+                int series = random.Next(2, 20);
+                int seasons = random.Next(1, 12);
+                return new Serial(series, seasons, RandomRating);
+            });
+
+            Fill(tvProgramCount, () =>
+            {
+                string time = $"{random.Next(0, 24):D2}:{random.Next(0, 60):D2}";
+                string start = $"{random.Next(0, 24):D2}:{random.Next(0, 60):D2}";
+                return new TVProgram(time, start, RandomRating);
+            });
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            BT_InitMovies(null, null);
+        }
+
+        //Достает одно кино из массива
+        private void BT_Take(object sender, RoutedEventArgs e)
+        {
+            if(movies.Count == 0)
+            {
+                MessageBox.Show("Кино закончились");
+                return;
+            }
+
+            int index = random.Next(0, movies.Count - 1);
+            if (movies[index] is Film)
+            {
+                filmCount--;               
+                L_FilmCount.Content = $"Фильмов: {filmCount}";
+            }
+            else if(movies[index] is Serial)
+            {
+                serialCount--;
+                L_SerialCount.Content = $"Сериалов: {serialCount}";
+            }
+            else if(movies[index] is TVProgram)
+            {
+                tvProgramCount--;
+                L_TvCount.Content = $"Передач: {tvProgramCount}";
+            }
+
+            BitmapImage image = new BitmapImage(new Uri("/Images/"+movies[index].Image, UriKind.Relative));
+            IMG_Icon.Source = image;
+            string item = movies[index].Print();
+            MovieLabel.Content = item;
+            movies.RemoveAt(index);
+        }
+
+        //Отображает задание
+        public void BT_Task(object sender, RoutedEventArgs e)
+        {
+            string task = "Должен быть 1 базовый класс, и 3 класса наследника. У базового класса должно быть, как минимум одно свойство передающиеся по наследству остальным, у каждого из классов наследников должно быть как минимум два уникальных свойства.\n";
+            string variant = 
+                "Для раздачи кино(рейтинг)\n" +
+                "Фильм(хронометраж, количество наград, тип(художественный, документальный и т.п.))\n" +
+                "Сериал(общее количество серий, количество сезонов)\n" +
+                "Телепередача(продолжительность, эфирное время)";
+            MessageBox.Show(task + variant, "Задание. Вариант 10");
+        }
+    }
+}
---
+++ Laba_4/Movies.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_4
+{
+    //Кино
+    abstract class Movie
+    {
+        protected float _rating;
+        abstract public string Print();
+        public string Image { get; protected set; }
+    }
+
+    //Фильм
+    class Film : Movie
+    {
+        public enum Genre
+        {
+            Documentary,
+            Artistic,
+            Adventure
+        }
+        private string _time;
+        private Genre _genre;
+        private string GenreName { get
+            {
+                switch (_genre)
+                {
+                    case Genre.Documentary:
+                        return "Документальный";
+                    case Genre.Artistic:
+                        return "Художественный";
+                    case Genre.Adventure:
+                        return "Приключение";
+                    default:
+                        throw new ArgumentException("Не существующий жанр");
+                }
+            } }
+        public Film(string time, Genre genre, float rating)
+        {
+            _time = time;
+            _genre = genre;
+            _rating = rating;
+
+            switch(_genre)
+            {
+                case Genre.Documentary:
+                    Image = "Documentary.png";
+                    break;
+                case Genre.Artistic:
+                    Image = "Artistic.png";
+                    break;
+                case Genre.Adventure:
+                    Image = "Adventure.png";
+                    break;
+            }
+        }
+        public override string Print()
+        {
+            return $"Фильм\nРейтинг: {_rating}\nВремя: {_time}\nЖанр {GenreName}";
+        }
+    }
+
+    //Сериал
+    class Serial : Movie
+    {
+        private int _series;
+        private int _seasons;
+        public Serial(int series, int seasons, float rating)
+        {
+            _series = series;
+            _seasons = seasons;
+            _rating = rating;
+            Image = "Serial.png";
+        }
+        public override string Print()
+        {
+            return $"Сериал\nРейтинг: {_rating}\nСезонов: {_seasons}\nСерий: {_series}";
+        }
+    }
+
+    //Передача
+    class TVProgram : Movie
+    {
+        private string _time;
+        private string _start;
+        public TVProgram(string time, string start, float rating)
+        {
+            _time = time;
+            _start = start;
+            _rating = rating;
+            Image = "Tvprogram.png";
+        }
+        public override string Print()
+        {
+            return $"ТВ Передача\nРейтинг: {_rating}\nДлительность: {_time}\nЭфирное время: {_start}";
+        }
+    }
+}
